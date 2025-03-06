25d324146e2b814d5f63a8ee7e32fdd2e98d986b
azizkaaa <109865579+azizkaaa@users.noreply.github.com>
2025-03-06 20:45:22 +0500

    favicon changed

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,6 +15,7 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Apex Development",
   description: "Девелопер нового поколения",
+  
 };
 
 export default function RootLayout({ children }) {
